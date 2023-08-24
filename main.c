7776c58a1a6958e251c2585f1bb6762b1f878c09
bojanangelis <bojanangeleski98@gmail.com>
2023-08-24 15:51:52 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void findMaxAscSubarray(int *array, int n, int *maxStart, int *maxLength) {
+    int i;
+    int start;
+    int length;
+    for (i = 0; i < n - 1; i++) {
+        start = i;
+        length = 1;
+        while (*(array + i) < *(array + i + 1)) {
+            ++i;
+            length++;
+        }
+        if (length > *maxLength) {
+            *maxLength = length;
+            *maxStart = start;
+        }
+    }
+
+}
+
+int main() {
+    int array[100];
+    int i, n;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", array + i);
+    }
+    int maxStart = 0;
+    int maxLength = 1;
+
+    findMaxAscSubarray(array, n, &maxStart, &maxLength);
+    for (i = maxStart; i < maxStart + maxLength; i++) {
+        printf("%d ", *(array + i));
+    }
+    return 0;
+}
+/*
+ *
+ * max length in array
+ * 9
+ * 2 3 1 4 7 12 7 9 1
+ */
